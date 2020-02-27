33a0d0a972bfa5185822a4b70ecf8c9eef0c8fd8
Grigory Alfyorov <loonacuse@gmail.com>
2020-02-27 21:00:07 +0500

    Add unit tests for VirtualMachine

---
+++ LoChip8.Tests/ConsoleBeeper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoChip8.Tests
+{
+    public class ConsoleBeeper : IBeeper
+    {
+        public void Beep()
+        {
+            Console.Beep();
+        }
+    }
+}
