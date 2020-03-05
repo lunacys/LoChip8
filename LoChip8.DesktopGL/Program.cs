14277a4def24f685c657eb010e033662fb6616fd
Grigory Alfyorov <loonacuse@gmail.com>
2020-03-05 22:45:49 +0500

    Fix Program class after renaming Game1

--- LoChip8.DesktopGL/Program.cs
+++ LoChip8.DesktopGL/Program.cs
@@ -7,7 +7,7 @@ namespace LoChip8.DesktopGL
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new GameRoot())
                 game.Run();
         }
     }
